d960c205d0c23481cf98b02f7b623ade681e239b
jillianatkins <jill.carolei@gmail.com>
2021-04-11 11:31:22 -0600

    Removed useContext
    Removed useContext from declaration

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,5 +1,5 @@
 // imr (short cut)
-import React, { useState, useContext } from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components'
 import {Redirect} from 'react-router-dom';
 
